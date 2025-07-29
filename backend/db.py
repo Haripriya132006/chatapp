f0ce2f62bc737ee23356701a0b9074ba0442cc7c
Haripriya132006 <144870747+Haripriya132006@users.noreply.github.com>
2025-07-30 00:15:21 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel,create_engine,Session
 DB_URL = "postgresql://postgres:vanihari123@db.qyvutnzcczunupmukaej.supabase.co:6543/postgres"
-engine=create_engine(DB_URL,echo=True)
+engine = create_engine(DB_URL, echo=True, pool_pre_ping=True, connect_args={"sslmode": "require"})
 
 def get_session():
     with Session(engine) as session:
