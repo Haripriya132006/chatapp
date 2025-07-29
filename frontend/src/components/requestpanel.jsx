5ee9437914427d2cd60b6e94746791bcd99a6a70
Haripriya132006 <144870747+Haripriya132006@users.noreply.github.com>
2025-07-29 22:07:13 +0530

    Update requestpanel.jsx

--- frontend/src/components/requestpanel.jsx
+++ frontend/src/components/requestpanel.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8000";
-
+const BASE_URL = "https://chatapp-backend.onrender.com";
 function RequestPanel({ currentUser, setChatPartner, setUserName }) {
   const [allowedUsers, setAllowedUsers] = useState([]);
   const [blockedUsers, setBlockedUsers] = useState([]);
