55f26e1668213cc23245c495d52a93cf77932826
unknown <haripriyaks13@gmail.com>
2025-08-11 21:56:53 +0530

    success

--- backend/main.py
+++ backend/main.py
@@ -113,11 +113,15 @@ class PendingRequest(BaseModel):
 
 @app.get("/pending-requests/{username}")
 def get_requests(username: str):
-    db = get_session()
-    print(db.list_collection_names())
+    # db = get_session()
+
+    for db in get_session():
+        return list(db["chatrequests"].find({}, {"_id": 0}))
+
+    # print(db.list_collection_names())
 
     # requests = db["chatrequests"].find({"to_user": username, "status": "pending"})
-    return {"meaw":"meawww"}
+    # return {"meaw":"meawww"}
     # result = []
     # for r in requests:
         # result.append(PendingRequest(
